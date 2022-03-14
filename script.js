1af13e1ed8267aeeb5fe3d56d4fbd7eefc53cd6c
Neema Velasco <neemavelasco@gmail.com>
2022-03-14 13:04:14 -0500

    added function to get weather using one call api and lat,lon

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const key = "66c87a27c099bc7c4716aa574eadefef";
 
-// one call api - exclude hourly & minutely 
-const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely&appid=${key}`
-
 function convertToCoordinates(cityname) {
     // convert city name to coordinates - needed to use one call api
     const geocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`
@@ -10,12 +7,21 @@ function convertToCoordinates(cityname) {
     fetch(geocodingUrl)
     .then(result => result.json())
     .then(data => {
-        console.log(data[0])
+        // console.log(data[0])
         const { lat, lon } = data[0];
-
-        console.log(lat + " " + lon)
+        
+        getWeather(lat,lon)
     })
 
+}
+
+function getWeather(lat,lon) {
+    // one call api - exclude hourly & minutely 
+    const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${key}`
+    
+    fetch(oneCallUrl)
+        .then(result => result.json())
+        .then(data => console.log(data))
 }
 
 
