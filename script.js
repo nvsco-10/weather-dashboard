ab7b40838b630d81d8a6589c8fec8365e698bf81
Neema Velasco <neemavelasco@gmail.com>
2022-03-14 12:31:58 -0500

    added key and API urls

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const key = "66c87a27c099bc7c4716aa574eadefef";
 
+const geocodingUrl = `api.openweathermap.org/data/2.5/weather?q={city name}&appid=${key}`
+// one call api - exclude hourly & minutely 
+const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely&appid=${key}`
