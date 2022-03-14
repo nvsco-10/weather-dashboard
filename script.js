9ce21375fa797396da5e87ad9442421b3c7c4959
Neema Velasco <neemavelasco@gmail.com>
2022-03-14 12:58:35 -0500

    replaced geocoding url with correct api, added function to convert city name to coordinates

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const key = "66c87a27c099bc7c4716aa574eadefef";
 
-const geocodingUrl = `api.openweathermap.org/data/2.5/weather?q={city name}&appid=${key}`
 // one call api - exclude hourly & minutely 
 const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely&appid=${key}`
+
+function convertToCoordinates(cityname) {
+    // convert city name to coordinates - needed to use one call api
+    const geocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`
+
+    fetch(geocodingUrl)
+    .then(result => result.json())
+    .then(data => {
+        console.log(data[0])
+        const { lat, lon } = data[0];
+
+        console.log(lat + " " + lon)
+    })
+
+}
+
+
+convertToCoordinates("london");
+
+
