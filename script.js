1af01844db40c9e0b83fb3204ec9d55e27cda5d2
Neema Velasco <neemavelasco@gmail.com>
2022-03-15 09:59:46 -0500

    refactor js file and removed jquery

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const cityOutput = document.querySelector(".city-name");
 const dateOutput = document.querySelector(".date");
 
 //current weather data selectors
-const tempOutput = document.querySelector(".temp");
+const tempOutput = document.querySelector(".current-temp");
 const descOutput = document.querySelector(".description")
 const uvOutput = document.querySelector(".uv-index");
 const humidityOutput = document.querySelector(".humidity");
 const windOutput = document.querySelector(".wind");
 const feelsLikeOutput = document.querySelector(".feels-like")
 
-const fiveDayOutput = document.querySelector(".five-day");
-
 const key = "66c87a27c099bc7c4716aa574eadefef";
+
+document.addEventListener("DOMContentLoaded", convertToCoordinates("New York"))
 
 searchBtn.addEventListener("click", function() {
     const cityname = input.value;
@@ -30,17 +30,19 @@ function convertToCoordinates(city) {
     .then(result => result.json())
     .then(data => {
         // console.log(data[0])
-        const { lat, lon } = data[0];
+        const { lat, lon, name} = data[0];
         
-        getWeatherData(lat,lon)
+        getWeatherData(lat,lon,name)
     })
 
 }
 
-function getWeatherData(lat,lon) {
+function getWeatherData(lat, lon, name) {
     // one call api - exclude hourly & minutely 
     // https://api.openweathermap.org/data/2.5/onecall?lat=51.5073&lon=-0.1276&exclude=hourly,minutely&appid=66c87a27c099bc7c4716aa574eadefef
     const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=${key}`
+
+    cityOutput.textContent = name;
     
     fetch(oneCallUrl)
         .then(result => result.json())
@@ -66,32 +68,22 @@ function displayWeatherData(data) {
     // 5-day forecast temperature
     get5Day(daily);
 
-
 }
 
 
 function get5Day(data) {
-    const currentDate = new Date();
-    fiveDayOutput.innerHtml = "";
+    
+    console.log(data)
 
-    for (let i=0; i<5; i++) {
-        const { temp, humidity, wind_speed, weather } = data[i];
-        const { icon } = weather[0];
-   
-        fiveDayOutput.append(
-            `<div class="day-box">
-                <span>January 3</span>
-                <div class="future-data">
-                    <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather-icon">
-                    <div>
-                        <span>${temp.day}°F</span>
-                        <p><span>${humidity}%</span> | <span>${wind_speed} mph</span></p>
-                    </div>
-                </div>
-            </div>`
-        )
+    for (let i=1; i<6; i++) {
+        const icon = data[i].weather[0].icon;
+        document.querySelector(`.day${i}-temp`).textContent = `${data[i].temp.day}°F`;
+        document.querySelector(`.day${i}-humidity`).textContent = `${data[i].humidity}%`;
+        document.querySelector(`.day${i}-wind`).textContent = `${data[i].wind_speed} mph`;
+        document.querySelector(`.day${i}-icon`).src = `http://openweathermap.org/img/wn/${icon}.png`;
     }
-        
+
+
 }   
 
 
@@ -108,7 +100,7 @@ function getCurrentDate() {
     const date = currentDate.toLocaleString('en-US', options);
 
     return date;
-}
+};
 
 // https://api.openweathermap.org/geo/1.0/direct?q=london&limit=5&appid=66c87a27c099bc7c4716aa574eadefef
 
@@ -117,4 +109,4 @@ function getCurrentDate() {
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=51.5073&lon=-0.1276&exclude=hourly,minutely&units=imperial&appid=66c87a27c099bc7c4716aa574eadefef
 
-//time stamp to date: https://www.delftstack.com/howto/javascript/javascript-convert-timestamp-to-date/
+// time stamp to date: https://www.delftstack.com/howto/javascript/javascript-convert-timestamp-to-date
