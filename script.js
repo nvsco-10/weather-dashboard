a130b2d95097cf6df208b488104214cd43286af2
Neema Velasco <neemavelasco@gmail.com>
2022-03-14 19:15:59 -0500

    updated display weather data function to replace selectors text content with live data

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const searchBtn = document.querySelector(".search-btn");
 const cityOutput = document.querySelector(".city-name");
 const dateOutput = document.querySelector(".date");
 const tempOutput = document.querySelector(".temp");
+const descOutput = document.querySelector(".description")
 const uvOutput = document.querySelector(".uv-index");
 const humidityOutput = document.querySelector(".humidity");
 const windOutput = document.querySelector(".wind");
@@ -11,7 +12,7 @@ const feelsLikeOutput = document.querySelector(".feels-like")
 
 const key = "66c87a27c099bc7c4716aa574eadefef";
 
-submitBtn.addEventListener("click", function() {
+searchBtn.addEventListener("click", function() {
     const cityname = input.value;
     console.log(cityname)
     convertToCoordinates(cityname);
@@ -35,7 +36,7 @@ function convertToCoordinates(city) {
 function getWeatherData(lat,lon) {
     // one call api - exclude hourly & minutely 
     // https://api.openweathermap.org/data/2.5/onecall?lat=51.5073&lon=-0.1276&exclude=hourly,minutely&appid=66c87a27c099bc7c4716aa574eadefef
-    const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${key}`
+    const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=${key}`
     
     fetch(oneCallUrl)
         .then(result => result.json())
@@ -47,16 +48,23 @@ function displayWeatherData(data) {
     console.log(current);
     console.log(daily);
 
-    //time stamp to date: https://www.delftstack.com/howto/javascript/javascript-convert-timestamp-to-date/
-    
-    // current weather data = data.current
-    // const { temp } = data.current;
-    // console.log("temp: " + temp)
+    // current weather data
+    const { temp, uvi, wind_speed, humidity, feels_like, weather } = current;
+    const { icon, description } = weather[0];
 
+    tempOutput.textContent = `${temp}°F`;
+    descOutput.textContent = description;
+    uvOutput.textContent = uvi;
+    humidityOutput.textContent = `${humidity}%`;
+    windOutput.textContent = `${wind_speed} mph`;
+    feelsLikeOutput.textContent = `${feels_like}°F`
 
-    // five data weather data = data.daily[0]
-    // console.log(data.daily[0])
 }
 
+
+    // https://api.openweathermap.org/data/2.5/onecall?lat=51.5073&lon=-0.1276&exclude=hourly,minutely&units=imperial&appid=66c87a27c099bc7c4716aa574eadefef
+
+    //time stamp to date: https://www.delftstack.com/howto/javascript/javascript-convert-timestamp-to-date/
+    
 
 
